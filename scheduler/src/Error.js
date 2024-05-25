a5070509f9ca053bd91d5620da58c4963b5e2a53
Shamik Ganguly <shamgang@umich.edu>
2024-05-25 08:11:09 -0700

    Update error page

--- scheduler/src/Error.js
+++ scheduler/src/Error.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { useNavigate, useRouteError } from "react-router-dom";
+import { useEffect } from 'react';
+import { useRouteError } from "react-router-dom";
 import { getRandomBackgroundImageUrl } from "./helpers/BackgroundImage";
 import { getLogURL } from './helpers/LogDownload';
 import './Error.css';
@@ -9,40 +9,23 @@ const BACKGROUND_STYLE = {
   backgroundImage: `url(${BACKGROUND_IMAGE})`
 };
 
-const REDIRECT_DELAY_S = 20;
-
 function ErrorPage() {
   const error = useRouteError();
-  const navigate = useNavigate();
-  const [countdown, setCountdown] = useState(REDIRECT_DELAY_S);
-
 
   // Initial setup
   useEffect(() => {
     console.error('Error page received error', error);
-    // Start countdown
-    const interval = setInterval(() => {
-      setCountdown(prev => {
-        if (prev <= 0) {
-          navigate('/');
-          return prev;
-        }
-        return prev - 1;
-      });
-    }, 1000);
-    // Cleanup
-    return () => clearInterval(interval);
-  }, [error, navigate, setCountdown]);
+  }, [error]);
 
   return (
     <div className="error-page" style={BACKGROUND_STYLE}>
       <div className="oops">
         <h1>OOPS</h1>
-        <h3>{error?.statusText}</h3>
+        <h3>{error?.statusText || 'UNKNOWN ERROR'}</h3>
         <p>
           {
             <span>
-              Click this link to download and send logs to the developer:<br/>
+              Click this link to download logs to send to the developer:<br/>
               <a
                 href={getLogURL()}
                 download="console_logs.txt"
@@ -50,8 +33,7 @@ function ErrorPage() {
                 console_logs.txt
               </a>
             </span>
-          }<br/>
-          Redirecting in {countdown}...
+          }
         </p>
       </div>
     </div>
